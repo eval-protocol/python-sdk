b95909b7422bfbd0866a742cd1e3b2a0b0e0fcd4
Dylan Huang <dhuang@fireworks.ai>
2025-08-03 12:44:37 -0700

    fix __init__.py

--- eval_protocol/pytest/__init__.py
+++ eval_protocol/pytest/__init__.py
@@ -1,8 +1,9 @@
 from .default_agent_rollout_processor import default_agent_rollout_processor
 from .default_no_op_rollout_process import default_no_op_rollout_processor
 from .default_single_turn_rollout_process import default_single_turn_rollout_processor
-from .pytest_utils import evaluate, evaluation_test
+from .evaluation_test import evaluation_test
 from .types import RolloutProcessor, RolloutProcessorConfig
+from .utils import evaluate
 
 __all__ = [
     "default_agent_rollout_processor",
