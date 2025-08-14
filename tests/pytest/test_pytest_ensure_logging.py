57a28d04e5848d806633c999323f88be187c50dd
Derek Xu <xzrderek@gmail.com>
2025-08-14 02:10:45 -0700

    remove import

--- tests/pytest/test_pytest_ensure_logging.py
+++ tests/pytest/test_pytest_ensure_logging.py
@@ -25,7 +25,6 @@ async def test_ensure_logging(monkeypatch):
         "eval_protocol.dataset_logger.sqlite_dataset_logger_adapter.SqliteEvaluationRowStore", return_value=mock_store
     ):
         from eval_protocol.models import EvaluationRow
-        from eval_protocol.pytest.default_no_op_rollout_process import default_no_op_rollout_processor
         from eval_protocol.pytest.evaluation_test import evaluation_test
         from tests.pytest.test_markdown_highlighting import markdown_dataset_to_evaluation_row
 
