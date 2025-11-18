5dacb42d4cfc1e014bb553470ba9efdd1ac286c7
zhengliu <lliuzzheng@gmail.com>
2025-11-17 17:22:49 -0800

    Add more use cases

--- tests/pytest/test_pytest_klavis_mcp.py
+++ tests/pytest/test_pytest_klavis_mcp.py
@@ -12,8 +12,10 @@ import os
 class ResponseFormat(BaseModel):
     score: float
 
-
-# You should copy https://painted-tennis-ebc.notion.site/MCPMark-Source-Hub-23181626b6d7805fb3a7d59c63033819 into your Notion for the notion test.
+'''
+You should copy https://painted-tennis-ebc.notion.site/MCPMark-Source-Hub-23181626b6d7805fb3a7d59c63033819
+into your Notion for the notion test.
+'''
 @evaluation_test(
     input_dataset=["tests/pytest/datasets/klavis_mcp_test.jsonl"],
     rollout_processor=AgentRolloutProcessor(),
